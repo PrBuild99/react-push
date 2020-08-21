1f3ae6e260b1a6a3a6ac9dc1ef7211362d708f94
Joshua <joshuaniteesh551@gmail.com>
2020-08-21 12:20:18 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb+srv://gg:gg@devconnector.bmwim.mongodb.net/devconnector",
+};
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const db = require("./config/keys").mongoURI;
+
+//Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Mongo Connected"))
+  .catch(() => console.log(error));
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log(`Server started on port ${port}`);
+});
